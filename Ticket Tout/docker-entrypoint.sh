0d83b328d859a17973290e2b9a893380ff834793
theo-futol <theo.futol@epitech.eu>
2026-09-04 15:13:24 +0400

    refactor: clean up entrypoint script and update environment variable handling

--- Ticket Tout/docker-entrypoint.sh
+++ Ticket Tout/docker-entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 ./migration.sh
 
-if [ -n "$APP_HOST_IP" ]; then
-  exec npx next start --hostname 0.0.0.0 --experimental-https \
-    --experimental-https-key "./certificates/${APP_HOST_IP}-key.pem" \
-    --experimental-https-cert "./certificates/${APP_HOST_IP}.pem"
-fi
+# if [ -n "$APP_HOST_IP" ]; then
+#   exec npx next start --hostname 0.0.0.0 --experimental-https \
+#     --experimental-https-key "./certificates/${APP_HOST_IP}-key.pem" \
+#     --experimental-https-cert "./certificates/${APP_HOST_IP}.pem"
+# fi
 
 exec "$@"
