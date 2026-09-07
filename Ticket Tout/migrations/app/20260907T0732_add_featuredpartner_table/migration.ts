f32a555b53409ef6b2dd9b323e5fb8f614f46dc9
brandon bègue <brandon-jean-samuel.begue@epitech.eu>
2026-09-07 11:36:08 +0400

    fix: prisma migrations

--- Ticket Tout/migrations/app/20260907T0732_add_featuredpartner_table/migration.ts
+++ Ticket Tout/migrations/app/20260907T0732_add_featuredpartner_table/migration.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S node
-import type { Contract as Start } from '../../snapshots/3aef406a4dac1916022111ec37bec1bfd8a96403932cf52c47106189b4ae6406/contract';
-import startContract from '../../snapshots/3aef406a4dac1916022111ec37bec1bfd8a96403932cf52c47106189b4ae6406/contract.json' with { type: 'json' };
-import type { Contract as End } from '../../snapshots/e95c0443e1fd7b6dcf356942fe5d00ca51c321b904f65238b02da3a331bdd3f3/contract';
-import endContract from '../../snapshots/e95c0443e1fd7b6dcf356942fe5d00ca51c321b904f65238b02da3a331bdd3f3/contract.json' with { type: 'json' };
+import type { Contract as End } from '../../snapshots/c65231faac1f674dc4f5664970871d5a9a2f35427c46207786fc0f58ef78d55c/contract';
+import endContract from '../../snapshots/c65231faac1f674dc4f5664970871d5a9a2f35427c46207786fc0f58ef78d55c/contract.json' with { type: 'json' };
+import type { Contract as Start } from '../../snapshots/d7dd146512eb4371934bcd70e7350ddedabb6179a77af8b4701bb1201bbafe9f/contract';
+import startContract from '../../snapshots/d7dd146512eb4371934bcd70e7350ddedabb6179a77af8b4701bb1201bbafe9f/contract.json' with { type: 'json' };
 import { Migration, MigrationCLI, col, fn, lit, primaryKey } from '@prisma/orm-postgres/migration';
 
 export default class M extends Migration<Start, End> {
